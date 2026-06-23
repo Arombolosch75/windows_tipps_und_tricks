26e3b27167e0546fd7d59f634bd2717d3ab31f9e
Arombolosch75 <99829083+Arombolosch75@users.noreply.github.com>
2026-06-23 20:12:34 +0200

    Remove loadGroup function and event listener

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-// app.js
-function loadGroup(groupName) {
-    const container = document.querySelector('.grid-container');
-    container.innerHTML = ''; // Leeren
 
-    const items = internetLinks[groupName] || [];
-    
-// In app.js innerhalb der forEach-Schleife:
-items.forEach(item => {
-    // Hier wird das Logo aus dem item-Objekt mitgegeben
-    const card = new Card(item.title, item.url, item.desc, item.logo);
-    container.innerHTML += card.render();
-});
-}
-
-// app.js
-document.addEventListener("DOMContentLoaded", () => {
-    loadGroup('social'); // Lädt die Daten beim Start
-});
